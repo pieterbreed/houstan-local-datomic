81d8e6e9aa5829e47246ddcac90066eb59afd2ac
Pieter Breed <pieter.breed@gmail.com>
2016-09-12 18:44:33 +0200

    save game

--- init.clj
+++ init.clj
@@ -3,7 +3,7 @@
 (set-env! :dependencies '[[pieterbreed/tappit "0.9.8"]
                           [me.raynes/conch "0.8.0"]
                           [environ "1.0.3"]
-                          [yoostan-lib.utils :as utils]])
+                          [pieterbreed/yoostan-lib "0.0.1-SNAPSHOT"]])
 
 
 ;; ----------------------------------------
@@ -21,6 +21,8 @@
 
 (require '[tappit.producer :refer [with-tap! ok]])
 (require '[me.raynes.conch :as conch])
+(require '[me.raynes.conch.low-level :as sh])
+(require '[yoostan-lib.utils :as utils])
 
 ;; This script will help the user figure out how to
 ;; configure the environment for this system to get
@@ -77,7 +79,7 @@
 
   ;; ----------------------------------------
 
-  (me.raynes.conch/programs git)
+  (conch/programs git)
 
   ;; ----------------------------------------
 
@@ -97,18 +99,35 @@
 
   (def houstan-library-git
     (or (environ.core/env :houstan-library-git)
-        "https://github.com/pieterbreed/houstan-library-local"))
-  (diag-lines ["You can override the houstan-library-git repository using"
-               "HOUSTAN_LIBRARY_GIT environment variable."
-               "Currently set to: "
-               houstan-library-git])
+        (do
+          (diag-lines ["You can override the HOUSTAN-LIBRARY-GIT repository using"
+                       "HOUSTAN_LIBRARY_GIT environment variable."])
+          "https://github.com/pieterbreed/houstan-library-local")))
+  
+
+  (def houstan-library-git-branch
+    (or (environ.core/env :houstan-library-git-branch)
+        (do
+          (diag-lines ["You can override the HOUSTAN-LIBRARY-GIT _branch_"
+                       "by specifying the HOUSTAN_LIBRARY_GIT_BRANCH"
+                       "environment variable."])
+          "0.0.2")))
 
   (git "clone" "--recursive"
+       "-b" houstan-library-git-branch
        houstan-library-git
        (clojure.java.io/file houstan-dir "library"))
   (ok! ok "cloned-houstan-library")
 
   ;; ----------------------------------------
 
+  (def vagrant-init-script-file (clojure.java.io/file houstan-dir "library" "houstan-local-vagrant" "init.clj"))
+  (diag! (str "looking for vagrant-init-script at " (.getCanonicalPath vagrant-init-script-file)))
+  (ok! (.exists vagrant-init-script-file)
+       "vagrant-init-script-exists"
+       :diag "If this fails, you have a broken library.")
 
-  )
+  (diag! "Calling vagrant init script. This might take some time...")
+  (conch/let-programs
+      [init (.getAbsolutePath vagrant-init-script-file)]
+    (init)))
