9c88818fbf9223a61c9c482a0004d5de5d1cbe6b
Pieter Breed <pieter.breed@gmail.com>
2016-09-26 16:11:03 +0200

    swapped over to ansible-invontery-clj

--- inventory.clj
+++ inventory.clj
@@ -1,12 +1,12 @@
 #!/usr/bin/env boot
 
-(set-env! :dependencies '[[pieterbreed/yoostan-lib "0.0.1-SNAPSHOT"]
+(set-env! :dependencies '[[pieterbreed/ansible-inventory-clj "0.1.1"]
                           [environ "1.0.3"]
                           [org.clojure/data.json "0.2.6"]])
 
 ;; ----------------------------------------
 
-(require '[yoostan-lib.inventory :as yinv])
+(require '[ansible-inventory-clj.core :as yinv])
 (require '[environ.core :as env])
 (require '[clojure.data.json :as json])
 
@@ -15,23 +15,23 @@
 
 (let [vagrant-host ["vagrant" (env/env :houstan-hostname) 22]] 
   (as-> yinv/empty-inventory $
-    (yinv/target-> $ vagrant-host {"ansible_user" "vagrant"
-                                   "ansible_host" (env/env :houstan-hostname)})
+    (yinv/add-target $ vagrant-host {"ansible_user" "vagrant"
+                                     "ansible_host" (env/env :houstan-hostname)})
 
-    (yinv/group-> $ "datomic-transactors" 
-                  {"datomic_transactor_properties"
-                   {"protocol" "dev"
-                    "host" "0.0.0.0"
-                    "alt-host" (env/env :houstan-hostname)
-                    "port" "4334"
-                    "license-key" (env/env :datomic-license-key)
-                    "memory-index-threshold" "32m"
-                    "memory-index-max" "256m"
-                    "object-cache-max" "128m"
-                    "data-dir" "/var/lib/datomic"
-                    "log-dir" "/var/log/datomic"
-                    "pid-file" "/var/run/datomic/datomic_transactor.pid"}})
-    (yinv/target->group-> $ vagrant-host "datomic-transactors")
-    (yinv/inv->ansible--list $)
+    (yinv/add-group $ "datomic-transactors" 
+                    {"datomic_transactor_properties"
+                     {"protocol" "dev"
+                      "host" "0.0.0.0"
+                      "alt-host" (env/env :houstan-hostname)
+                      "port" "4334"
+                      "license-key" (env/env :datomic-license-key)
+                      "memory-index-threshold" "32m"
+                      "memory-index-max" "256m"
+                      "object-cache-max" "128m"
+                      "data-dir" "/var/lib/datomic"
+                      "log-dir" "/var/log/datomic"
+                      "pid-file" "/var/run/datomic/datomic_transactor.pid"}})
+    (yinv/add-target-to-group $ vagrant-host "datomic-transactors")
+    (yinv/make-ansible-list $)
     (json/write-str $)
     (println $)))
